b1edb4b2be4b0a0344a2dd1d5f7c0b494a51fd4b
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-07-03 14:08:15 +0800

    Merge upstream (#7)

--- undetected_playwright/__init__.py
+++ undetected_playwright/__init__.py
@@ -5,5 +5,5 @@
 # Description:
 from .ninja import StealthConfig, stealth_async, stealth_sync
 
-__version__ = "0.0.5"
+__version__ = "0.1.0"
 __all__ = ["StealthConfig", "stealth_async", "stealth_sync"]
